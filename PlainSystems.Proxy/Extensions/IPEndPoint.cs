c594343990cd2e285282f57971752f268efbe975
Friedrich Kräft <fraftdev@outlook.de>
2022-08-04 17:30:20 +0200

    Projektdateien hinzufügen.

---
+++ PlainSystems.Proxy/Extensions/IPEndPoint.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlainSystems.Proxy.Extensions
+{
+    public class IPEndPoint : System.Net.IPEndPoint
+    {
+        public IPEndPoint(long address, int port) : base(address, port) { }
+        public IPEndPoint(IPAddress address, int port) : base(address, port) { }
+
+        public static bool TryParse(string value, out IPEndPoint result)
+        {
+            if (!Uri.TryCreate($"tcp://{value}", UriKind.Absolute, out Uri uri) ||
+                !IPAddress.TryParse(uri.Host, out IPAddress ipAddress) ||
+                uri.Port < 0 || uri.Port > 65535)
+            {
+                result = default(IPEndPoint);
+                return false;
+            }
+
+            result = new IPEndPoint(ipAddress, uri.Port);
+            return true;
+        }
+    }
+}
